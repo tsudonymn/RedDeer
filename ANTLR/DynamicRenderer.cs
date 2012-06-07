2c67cf19435c2c3f3b6a21ecc99927072287185e
U-ISR\msms <msms@MSMSu0001816.ad.isr.umich.edu>
2012-06-07 12:00:31 -0400

    Cleaning attempt #2

--- ANTLR/DynamicRenderer.cs
+++ ANTLR/DynamicRenderer.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Dynamic;
+using System.Windows.Forms;
 
 namespace ANTLR
 {
@@ -11,42 +12,94 @@ namespace ANTLR
         {
             this.datagroup = datagroup;
         }
-        
+
+        public override bool TryGetIndex(GetIndexBinder binder, object[] indexes, out object result)
+        {
+            MessageBox.Show("Here");
+            return base.TryGetIndex(binder, indexes, out result);
+        }
+
+        public override bool TryBinaryOperation(BinaryOperationBinder binder, object arg, out object result)
+        {
+            MessageBox.Show("Here");
+            return base.TryBinaryOperation(binder, arg, out result);
+        }
+
+        public override bool TryConvert(ConvertBinder binder, out object result)
+        {
+            MessageBox.Show("Here");
+            return base.TryConvert(binder, out result);
+        }
+
+        public override bool TryCreateInstance(CreateInstanceBinder binder, object[] args, out object result)
+        {
+            MessageBox.Show("Here");
+            return base.TryCreateInstance(binder, args, out result);
+        }
+
+        public override bool TryDeleteIndex(DeleteIndexBinder binder, object[] indexes)
+        {
+            MessageBox.Show("Here");
+            return base.TryDeleteIndex(binder, indexes);
+        }
+
+        public override bool TryDeleteMember(DeleteMemberBinder binder)
+        {
+            MessageBox.Show("Here");
+            return base.TryDeleteMember(binder);
+        }
+
+        public override bool TryInvoke(InvokeBinder binder, object[] args, out object result)
+        {
+            MessageBox.Show("Here");
+            return base.TryInvoke(binder, args, out result);
+        }
+
+        public override bool TrySetIndex(SetIndexBinder binder, object[] indexes, object value)
+        {
+            MessageBox.Show("Here");
+            return base.TrySetIndex(binder, indexes, value);
+        }
+
+        public override bool TryUnaryOperation(UnaryOperationBinder binder, out object result)
+        {
+            MessageBox.Show("Here");
+            return base.TryUnaryOperation(binder, out result);
+        }
+
         public override bool TryGetMember(GetMemberBinder binder, out object result)
+        {
+            string methodNameThatWasCalled = binder.Name;
+            result = checkForAttribute(methodNameThatWasCalled);
+            return true;
+        }
+
+        public override bool TryInvokeMember(InvokeMemberBinder binder, object[] args, out object result)
+        {
+            string methodNameThatWasCalled = binder.Name;
+            result = checkForAttribute(methodNameThatWasCalled);
+            return true;
+        }
+
+        public override bool TrySetMember(SetMemberBinder binder, object value)
         {
             string methodNameThatWasCalled = binder.Name;
             EntityAttribute entityAttribute = datagroup.Attributes.Find(a => a.Name.Equals(methodNameThatWasCalled));
             if (entityAttribute != null)
             {
-                result = entityAttribute.Value;
-                return true;
-            }
-            result = String.Empty;
-            return true;
-        }
-//        public override bool TryInvokeMember(InvokeMemberBinder binder, object[] args, out object result)
-//        {
-//            string name = binder.Name;
-//            EntityAttribute entityAttribute = datagroup.Attributes.Find(a => a.Name.Equals(name));
-//            if (entityAttribute != null)
-//            {
-//                result = entityAttribute.Value;
-//                return true;
-//            }
-//            result = String.Empty;
-//            return false;
-//        }
-//
-        public override bool TrySetMember(
-            SetMemberBinder binder, object value)
-        {
-            string name = binder.Name;
-            EntityAttribute entityAttribute = datagroup.Attributes.Find(a => a.Name.Equals(name));
-            if (entityAttribute != null)
-            {
                 entityAttribute.Value = value.ToString();
             }
             return true;
+        }
+
+        private object checkForAttribute(string methodNameThatWasCalled)
+        {
+            EntityAttribute entityAttribute = datagroup.Attributes.Find(a => a.Name.Equals(methodNameThatWasCalled));
+            if (entityAttribute != null)
+            {
+                return entityAttribute.Value;
+            }
+            return String.Empty;
         }
     }
 }
--- ANTLRTests/DynamicRendererTest.cs
+++ ANTLRTests/DynamicRendererTest.cs
@@ -1,4 +1,8 @@
-﻿using ANTLR;
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Dynamic;
+using ANTLR;
 using NUnit.Framework;
 using Antlr4.StringTemplate;
 
@@ -8,15 +12,36 @@ namespace ANTLRTests
     [TestFixture]
     public class DynamicRendererTest
     {
+        [Test]
+        public void NestedObjectDictionaryWithStringTemplate()
+        {
+            var tmp = new { parent = new { child = new Dictionary<string, string> { { "foo", "I should get this" }, { "bar", "And This" } } } };
+
+            Template template = new Template("<x.parent.child.foo>&<x.parent.child.bar>");
+
+            template.Add("x", tmp);
+            Assert.That(template.Render(), Is.EqualTo(tmp.parent.child["foo"] + "&" + tmp.parent.child["bar"]));
+        }
+
+        [Test]
+        public void NestedObjectsWithStringTemplate()
+        {
+            var tmp = new { parent = new { child = new { foo = "I should get this"} } };
+
+            Template template = new Template("<x.parent.child.foo>");
+
+            template.Add("x", tmp);
+            Assert.That(template.Render(), Is.EqualTo(tmp.parent.child.foo));
+        }
 
         [Test]
         public void ObjectsWithStringTemplate()
         {
-            dynamic tmp = new {foo = "VS Sux!"};
+            dynamic tmp = new { foo = "I should get this" };
             Template template = new Template("<x.foo>");
 
             template.Add("x", tmp);
-            Assert.That(template.Render(), Is.EqualTo(tmp.foo) );
+            Assert.That(template.Render(), Is.EqualTo(tmp.foo));
         }
 
         [Test]
@@ -30,15 +55,39 @@ namespace ANTLRTests
         }
 
         [Test]
-        public void DynamicObjectWithStringTemplate()
+        public void DynamicObjectWithStringTemplateDoesNotAppearToWork()
+        {
+            Datagroup datagroup = new Datagroup();
+            EntityAttribute entityAttribute = new EntityAttribute { Name = "foo", Value = "Why don't I get this?" };
+            datagroup.Attributes.Add(entityAttribute);
+            dynamic dynamicRenderer = new DynamicRenderer(datagroup);
+
+            var expando = new ExpandoObject();
+            var expandoDictionary = (IDictionary<string, object>)expando;
+            expandoDictionary["foo"] = "something";
+            Dictionary<string, string> dd = new Dictionary<string, string>();
+            dd["foo"] = "something";
+            Template template = new Template("<x.foo>");
+            template.Add("x", dynamicRenderer);
+
+            Assert.That(template.Render(), Is.EqualTo(entityAttribute.Value));
+        }
+
+        [Test]
+        public void CustomDictionayImplementationWithStringTemplate()
         {
             Datagroup datagroup = new Datagroup();
             EntityAttribute entityAttribute = new EntityAttribute { Name = "foo", Value = "I hate VS" };
             datagroup.Attributes.Add(entityAttribute);
-            dynamic dynamicRenderer = new DynamicRenderer(datagroup);
+            var dictionary = new DatagroupAttributeDictionary(datagroup);
 
+            var expando = new ExpandoObject();
+            var expandoDictionary = (IDictionary<string, object>)expando;
+            expandoDictionary["foo"] = "something";
+            Dictionary<string, string> dd = new Dictionary<string, string>();
+            dd["foo"] = "something";
             Template template = new Template("<x.foo>");
-            template.Add("x", dynamicRenderer);
+            template.Add("x", dictionary);
 
             Assert.That(template.Render(), Is.EqualTo(entityAttribute.Value));
         }
@@ -81,6 +130,108 @@ namespace ANTLRTests
             renderer.foo = updatedValue;
             Assert.AreEqual(updatedValue, datagroup.Attributes.Find(a => a.Name.Equals(expectedAttributeName)).Value);
             Assert.AreEqual(updatedValue, renderer.foo);
+        }
+    }
+
+    public class DatagroupAttributeDictionary : IDictionary<string, string>
+    {
+        private readonly Datagroup datagroup;
+
+        public DatagroupAttributeDictionary(Datagroup datagroup)
+        {
+            this.datagroup = datagroup;
+        }
+
+        public IEnumerator<KeyValuePair<string, string>> GetEnumerator()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        public void Add(KeyValuePair<string, string> item)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Clear()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public bool Contains(KeyValuePair<string, string> item)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void CopyTo(KeyValuePair<string, string>[] array, int arrayIndex)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public bool Remove(KeyValuePair<string, string> item)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public int Count
+        {
+            get { throw new System.NotImplementedException(); }
+        }
+
+        public bool IsReadOnly
+        {
+            get { throw new System.NotImplementedException(); }
+        }
+
+        public bool ContainsKey(string key)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Add(string key, string value)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public bool Remove(string key)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public bool TryGetValue(string key, out string value)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public string this[string key]
+        {
+            get { return checkForAttribute(key).ToString(); }
+            set { throw new System.NotImplementedException(); }
+        }
+
+        public ICollection<string> Keys
+        {
+            get { throw new System.NotImplementedException(); }
+        }
+
+        public ICollection<string> Values
+        {
+            get { throw new System.NotImplementedException(); }
+        }
+
+
+        private object checkForAttribute(string methodNameThatWasCalled)
+        {
+            EntityAttribute entityAttribute = datagroup.Attributes.Find(a => a.Name.Equals(methodNameThatWasCalled));
+            if (entityAttribute != null)
+            {
+                return entityAttribute.Value;
+            }
+            return String.Empty;
         }
     }
 }
